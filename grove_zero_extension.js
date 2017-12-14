b1491cc9310d10ed828467c5063be3325a4e8949
jinyuan.weng <jinyuan.weng@seeed.cc>
2017-12-14 13:56:34 +0800

    Update

--- grove_zero_extension.js
+++ grove_zero_extension.js
@@ -1,75 +1,140 @@
+// picoExtension.js
+// Shane M. Clements, February 2014
+// PicoBoard Scratch Extension
+//
+// This is an extension for development and testing of the Scratch Javascript Extension API.
+
 (function(ext) {
-  
-  var pingCmd = new Uint8Array(1);
-  pingCmd[0] = 1;
-  
-  ext.whenButtonPressed = function() {
-      return false
-  }
-  
-  ext._getStatus = function() {
-    if (!connected)
-      return { status:1, msg:'Disconnected' };
-    else
-      return { status:2, msg:'Connected' };
-  };
+    var device = null;
+    var rawData = null;
 
-  ext._deviceRemoved = function(dev) {
-    console.log('Device removed');
-    // Not currently implemented with serial devices
-  };
+    // Buttn states:
+    var channels = {
+        'Button A': 0,
+        'Button B': 1
+    };
+    
+    var inputs = {
+        'Button A': 0,
+        'Button B': 0
+    };
 
-  var potentialDevices = [];
-  ext._deviceConnected = function(dev) {
-    potentialDevices.push(dev);
-    if (!device)
-      tryNextDevice();
-  };
+    ext.resetAll = function(){};
 
-  var poller = null;
-  var watchdog = null;
-  function tryNextDevice() {
-    device = potentialDevices.shift();
-    if (!device) return;
+    // Hats / triggers
+    ext.whenButtonPressed = function(which) {
+        return getButtonPressed(which);
+    };
 
-    device.open({ stopBits: 0, bitRate: 57600, ctsFlowControl: 0 });
-    console.log('Attempting connection with ' + device.id);
-    device.set_receive_handler(function(data) {
-      var inputData = new Uint8Array(data);
-      console.log('Input data:' + inputData);
-    });
+    // Private logic
+    function getButtonPressed(which) {
+        if (device == null) return false;
+        if (which == 'A' && getButton('Button A')) return true;
+        if (which == 'B' && getButton('Button B')) return true;
+        return false;
+    }
 
-    poller = setInterval(function() {
-      device.send(pingCmd.buffer); 
-    }, 1000);
+    function getButton(which) {
+        return inputs[which];
+    }
 
-    watchdog = setTimeout(function() {
-      clearInterval(poller);
-      poller = null;
-      device.set_receive_handler(null);
-      device.close();
-      device = null;
-      tryNextDevice();
-    }, 5000);
-  }
+    var inputArray = [];
+    function processData() {
+        var bytes = new Uint8Array(rawData);
+        
+        inputArray[3] = 0;
 
-  ext._shutdown = function() {
-    // TODO: Bring all pins down
-    if (device) device.close();
-    if (poller) clearInterval(poller);
-    device = null;
-  };
+        if (watchdog && (inputArray[3] == 0x54)) {
+            // Seems to be a valid board.
+            clearTimeout(watchdog);
+            watchdog = null;
+        }
 
-  var descriptor = {
-    blocks: [
+        console.log(bytes);
+        rawData = null;
+    }
+
+    function appendBuffer(buffer1, buffer2) {
+        var tmp = new Uint8Array(buffer1.byteLength + buffer2.byteLength);
+        tmp.set(new Uint8Array(buffer1), 0);
+        tmp.set(new Uint8Array(buffer2), buffer1.byteLength);
+        return tmp.buffer;
+    }
+
+    // Extension API interactions
+    var potentialDevices = [];
+    ext._deviceConnected = function(dev) {
+        potentialDevices.push(dev);
+
+        if (!device) {
+            tryNextDevice();
+        }
+    }
+
+    var poller = null;
+    var watchdog = null;
+    function tryNextDevice() {
+        // If potentialDevices is empty, device will be undefined.
+        // That will get us back here next time a device is connected.
+        device = potentialDevices.shift();
+        if (!device) return;
+
+        device.open({ stopBits: 0, bitRate: 38400, ctsFlowControl: 0 });
+        device.set_receive_handler(function(data) {
+            console.log('Received: ' + data.byteLength);
+            if(!rawData || rawData.byteLength == 4) rawData = new Uint8Array(data);
+            else rawData = appendBuffer(rawData, data);
+
+            if(rawData.byteLength >= 4) {
+                console.log(rawData);
+                processData();
+            }
+        });
+
+        // Tell the PicoBoard to send a input data every 50ms
+        var pingCmd = new Uint8Array(1);
+        pingCmd[0] = 1;
+        poller = setInterval(function() {
+            device.send(pingCmd.buffer);
+        }, 50);
+        watchdog = setTimeout(function() {
+            // This device didn't get good data in time, so give up on it. Clean up and then move on.
+            // If we get good data then we'll terminate this watchdog.
+            clearInterval(poller);
+            poller = null;
+            device.set_receive_handler(null);
+            device.close();
+            device = null;
+            tryNextDevice();
+        }, 250);
+    };
+
+    ext._deviceRemoved = function(dev) {
+        if(device != dev) return;
+        if(poller) poller = clearInterval(poller);
+        device = null;
+    };
+
+    ext._shutdown = function() {
+        if(device) device.close();
+        if(poller) poller = clearInterval(poller);
+        device = null;
+    };
+
+    ext._getStatus = function() {
+        if(!device) return {status: 1, msg: 'Disconnected'};
+        if(watchdog) return {status: 1, msg: 'Probing'};
+        return {status: 2, msg: 'Connected'};
+    }
+
+    var descriptor = {
+        blocks: [
             ['h', 'when %m.btns button pressed', 'whenButtonPressed', 'A']
         ],
         menus: {
-            btns: ['A', 'B', 'any']
+            btns: ['A', 'B']
         },
-        url: 'https://www.seeedstudio.com/'
-  };
-
-  ScratchExtensions.register('GroveZero', descriptor, ext, {type:'serial'});
-
+        url: 'www.seeedstudio.com'
+    };
+    ScratchExtensions.register('Grove Zero', descriptor, ext, {type: 'serial'});
 })({});
